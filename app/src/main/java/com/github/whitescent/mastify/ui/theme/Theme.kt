c458e525f18d7715de85c77d4508dbf97a0db170
whitescent <Nthily@outlook.com>
2023-08-28 12:25:18 +0800

    feat: remove TextField handle color

--- app/src/main/java/com/github/whitescent/mastify/ui/theme/Theme.kt
+++ app/src/main/java/com/github/whitescent/mastify/ui/theme/Theme.kt
@@ -2,9 +2,13 @@ package com.github.whitescent.mastify.ui.theme
 
 import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -23,7 +27,15 @@ fun MastifyTheme(
       WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
     }
   }
-  content.invoke()
+  val customTextSelectionColors = TextSelectionColors(
+    handleColor = Color.Transparent,
+    backgroundColor = Color.Transparent,
+  )
+  CompositionLocalProvider(
+    LocalTextSelectionColors provides customTextSelectionColors,
+  ) {
+    content.invoke()
+  }
   LaunchedEffect(darkTheme) {
     if (darkTheme) colorScheme.toggleToDarkColor() else colorScheme.toggleToLightColor()
   }
