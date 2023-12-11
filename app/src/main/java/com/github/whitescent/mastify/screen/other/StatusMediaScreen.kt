8722c579fa8f52f1e4d6ba561ba39dc676bf3509
whitescent <Nthily@outlook.com>
2023-12-11 12:26:49 +0800

    fix: zoom image

--- app/src/main/java/com/github/whitescent/mastify/screen/other/StatusMediaScreen.kt
+++ app/src/main/java/com/github/whitescent/mastify/screen/other/StatusMediaScreen.kt
@@ -20,12 +20,11 @@ package com.github.whitescent.mastify.screen.other
 import android.annotation.SuppressLint
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.CircularProgressIndicator
@@ -48,6 +47,7 @@ import com.github.fengdai.compose.media.ResizeMode
 import com.github.fengdai.compose.media.ShowBuffering
 import com.github.fengdai.compose.media.SurfaceType
 import com.github.fengdai.compose.media.rememberMediaState
+import com.github.panpf.zoomimage.ZoomImage
 import com.github.whitescent.mastify.AppNavGraph
 import com.github.whitescent.mastify.network.model.status.Status.Attachment
 import com.github.whitescent.mastify.ui.component.CenterRow
@@ -55,7 +55,7 @@ import com.github.whitescent.mastify.ui.component.player.ExoPlayerLifecycleEvent
 import com.github.whitescent.mastify.ui.component.player.rememberExoPlayerInstance
 import com.github.whitescent.mastify.ui.component.player.rememberPositionState
 import com.github.whitescent.mastify.ui.component.status.MediaType
-import com.mxalbert.zoomable.Zoomable
+import com.github.whitescent.mastify.ui.theme.AppTheme
 import com.ramcosta.composedestinations.annotation.Destination
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -76,86 +76,85 @@ fun StatusMediaScreen(
   val position = rememberPositionState(exoPlayer)
 
   ExoPlayerLifecycleEvents(exoPlayer)
-  Box(
-    modifier = Modifier
-      .fillMaxSize()
-      .background(Color.Black)
-  ) {
-    HorizontalPager(
-      state = pagerState,
-      pageContent = {
-        val mediaItem = attachments[it]
-        Zoomable(
-          modifier = Modifier.fillMaxSize()
-        ) {
-          when (MediaType.fromString(mediaItem.type)) {
-            MediaType.IMAGE -> {
-              val painter = rememberAsyncImagePainter(
-                model = ImageRequest.Builder(LocalContext.current)
-                  .data(mediaItem.url)
-                  .size(Size.ORIGINAL)
-                  .transformations()
-                  .build()
-              )
-              if (painter.state is AsyncImagePainter.State.Success) {
-                val size = painter.intrinsicSize
-                Image(
-                  painter = painter,
-                  contentDescription = null,
-                  modifier = Modifier
-                    .aspectRatio(size.width / size.height)
-                    .fillMaxSize()
-                )
-              }
-            }
 
-            MediaType.VIDEO -> {
-              Media(
-                state = mediaState,
-                resizeMode = ResizeMode.Fit,
-                showBuffering = ShowBuffering.Always,
-                buffering = {
-                  Box(Modifier.fillMaxSize(), Alignment.Center) {
-                    CircularProgressIndicator(color = Color.White, strokeWidth = 2.dp)
-                  }
-                },
-                surfaceType = SurfaceType.TextureView,
-              ) {
-                Crossfade(mediaState.isControllerShowing, Modifier.fillMaxSize()) { showing ->
-                  when (showing) {
-                    true -> {
-                      Box(
-                        modifier = Modifier
-                          .fillMaxSize()
-                          .padding(24.dp),
-                        contentAlignment = Alignment.BottomStart
-                      ) {
-                        CenterRow {
-                          Text(
-                            text = "position: $position",
-                            color = Color.White,
-                          )
-                        }
-                      }
-                    }
-                    else -> Unit
-                  }
-                }
-              }
-              LaunchedEffect(Unit) {
-                exoPlayer.run {
-                  repeatMode = Player.REPEAT_MODE_ONE
-                  setMediaItem(
-                    MediaItem.Builder().setMediaId(mediaItem.url).setUri(mediaItem.url).build()
-                  )
-                  prepare()
-                }
+  HorizontalPager(
+    state = pagerState,
+    modifier = Modifier.fillMaxSize().background(Color.Black),
+    pageContent = {
+      val mediaItem = attachments[it]
+      when (MediaType.fromString(mediaItem.type)) {
+        MediaType.IMAGE -> {
+          val painter = rememberAsyncImagePainter(
+            model = ImageRequest.Builder(LocalContext.current)
+              .data(mediaItem.url)
+              .size(Size.ORIGINAL)
+              .transformations()
+              .build()
+          )
+          when (painter.state) {
+            is AsyncImagePainter.State.Success -> {
+              ZoomImage(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+              )
+            }
+            is AsyncImagePainter.State.Loading -> {
+              Box(Modifier.fillMaxSize()) {
+                CircularProgressIndicator(
+                  color = AppTheme.colors.accent,
+                  modifier = Modifier.align(Alignment.Center)
+                )
               }
             }
             else -> Unit
           }
         }
-      },
-    )
-  }
+        MediaType.VIDEO -> {
+          Media(
+            state = mediaState,
+            resizeMode = ResizeMode.Fit,
+            showBuffering = ShowBuffering.Always,
+            buffering = {
+              Box(Modifier.fillMaxSize(), Alignment.Center) {
+                CircularProgressIndicator(color = Color.White, strokeWidth = 2.dp)
+              }
+            },
+            surfaceType = SurfaceType.TextureView,
+          ) {
+            Crossfade(mediaState.isControllerShowing, Modifier.fillMaxSize()) { showing ->
+              when (showing) {
+                true -> {
+                  Box(
+                    modifier = Modifier
+                      .fillMaxSize()
+                      .padding(24.dp),
+                    contentAlignment = Alignment.BottomStart
+                  ) {
+                    CenterRow {
+                      Text(
+                        text = "position: $position",
+                        color = Color.White,
+                      )
+                    }
+                  }
+                }
+                else -> Unit
+              }
+            }
+          }
+          LaunchedEffect(Unit) {
+            exoPlayer.run {
+              repeatMode = Player.REPEAT_MODE_ONE
+              setMediaItem(
+                MediaItem.Builder().setMediaId(mediaItem.url).setUri(mediaItem.url).build()
+              )
+              prepare()
+            }
+          }
+        }
+        else -> Unit
+      }
+    },
+  )
 }
