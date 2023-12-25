7015b9b597d911149d86887093f2a1ddbde41026
whitescent <Nthily@outlook.com>
2023-12-25 14:39:14 +0800

    build: upgrade app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
     applicationId = "com.github.whitescent.mastify"
     minSdk = 21
     targetSdk = 34
-    versionCode = 9
-    versionName = "1.2.0-pre-alpha"
+    versionCode = 10
+    versionName = "1.2.5-pre-alpha"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables {
       useSupportLibrary = true
