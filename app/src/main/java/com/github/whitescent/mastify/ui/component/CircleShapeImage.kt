74cd8f78acc2ccab5deda7b8771bb8f295557260
lazzzis <lazzzis@outlook.com>
2024-02-03 17:20:41 -0800

    fix: fix background color of a PNG avatar

--- app/src/main/java/com/github/whitescent/mastify/ui/component/CircleShapeImage.kt
+++ app/src/main/java/com/github/whitescent/mastify/ui/component/CircleShapeImage.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
@@ -37,12 +38,14 @@ fun CircleShapeAsyncImage(
   contentScale: ContentScale = ContentScale.Fit,
   alpha: Float = 1f,
   colorFilter: ColorFilter? = null,
-  onClick: (() -> Unit)? = null
+  onClick: (() -> Unit)? = null,
+  color: Color = AppTheme.colors.background
 ) {
   Surface(
     modifier = modifier,
     shape = shape,
-    border = border
+    border = border,
+    color = color
   ) {
     AsyncImage(
       model = model,
