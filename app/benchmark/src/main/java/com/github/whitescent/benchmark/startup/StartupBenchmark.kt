7ecf634984842758fdd85ded282f917a0e434078
lazzzis <lazzzis@outlook.com>
2024-03-20 23:37:58 -0700

    ci: fix failures

--- app/benchmark/src/main/java/com/github/whitescent/benchmark/startup/StartupBenchmark.kt
+++ app/benchmark/src/main/java/com/github/whitescent/benchmark/startup/StartupBenchmark.kt
@@ -32,7 +32,6 @@ import com.github.whitescent.benchmark.utils.waitForObject
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.lang.IllegalStateException
 
 @RunWith(AndroidJUnit4::class)
 class StartupBenchmark {
@@ -69,23 +68,19 @@ class StartupBenchmark {
           2000
         )
 
-        // Skip chrome's login screen, which is launched when chrome has never been used before
+        device.waitForIdle()
         try {
-          device.waitForObject(By.text("Use without an account"), 3000L)
-          device.findObject(By.text("Use without an account")).click()
-        } catch (e:IllegalStateException) {}
-          catch (e:NullPointerException) {}
-
-        try {
-          device.waitForObject(By.hint("电子邮件地址"), 5000L).text = username
+          // User might have logged in web app.
+          device.waitForObject(By.hint("电子邮件地址"), 10000L).text = username
           device.waitForObject(By.hint("密码"), 5000L).text = password
-          device.findObject(By.text("登录")).click()
-        } catch (e: IllegalStateException) {}
+          device.waitForObject(By.text("登录"), 1000L).click()
+        } catch (_: IllegalStateException) {}
 
-        device.waitForObject(By.text("同意授权"), 5000L)
-        device.findObject(By.text("同意授权")).click()
+        device.waitForIdle()
+        device.waitForObject(By.text("同意授权"), 10000L).click()
 
-        device.waitForObject(By.res("home timeline"), 5000L)
+        device.waitForIdle()
+        device.waitForObject(By.res("home timeline"), 10000L)
       }
 
       val column = device.findObject(By.res("home timeline"))
