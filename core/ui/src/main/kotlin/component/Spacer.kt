4d965a6dd238ad3ac4e179d3c5c3a07fd142cb1a
WhiteScent <rouxicoc@outlook.com>
2024-06-23 20:59:59 +0800

    chore: merge WidthSpacer and HeightSpacer

--- core/ui/src/main/kotlin/component/Spacer.kt
+++ core/ui/src/main/kotlin/component/Spacer.kt
@@ -18,12 +18,18 @@
 package com.github.whitescent.mastify.core.ui.component
 
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 
 @Composable
+fun HeightSpacer(
+  value: Dp
+) = Spacer(modifier = Modifier.height(value))
+
+@Composable
 fun WidthSpacer(
   value: Dp
-) = Spacer(Modifier.padding(horizontal = value))
+) = Spacer(Modifier.width(value))
